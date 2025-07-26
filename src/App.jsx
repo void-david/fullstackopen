3ca42b773c2b05a24251ef505e2fb219a3ada24c
David A. Lozano <107713321+void-david@users.noreply.github.com>
2025-07-26 02:58:30 -0600

    part 1 d done

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,48 @@
 import { useState } from 'react'
 
-const Button = ({onClick, text}) => {
-  return <button onClick={onClick}>{text}</button>
-}
 
-const Statistics = ({good, bad, neutral}) => {
-  if(good==0 && bad==0 && neutral==0){
-    return <p>No feedback given</p>
-  }
-
-  return(
-    <>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {good+neutral+bad}</p>
-      <p>Average: {(good-bad)/(good+bad+neutral)}</p>
-      <p>Positive: {good/(good+bad+neutral)}%</p>
-    </>
-  )
-}
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [votes, setVotes] = useState([0,0,0,0,0,0,0,0])
+   
+  const [selected, setSelected] = useState(0)
+
+  const handleVote = (votes, selected) => {
+    const newVotes = votes.map((vote, index) =>{
+      if(index===selected){
+        return vote+1;
+      }
+      return vote;
+    })
+    setVotes(newVotes)
+  }
+
+  const handleNext = () => {
+    let max = 7;
+    let min = 0;
+    let random = Math.floor(Math.random() * (max - min + 1)) + min;
+    setSelected(random)
+  }
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button onClick={()=>setGood(good + 1)} text={'good'}/>
-      <Button onClick={()=>setNeutral(neutral + 1)} text={'neutral'}/>
-      <Button onClick={()=>setBad(bad + 1)} text={'bad'}/>
-
-
-      <h1>Statistics</h1>
-      <Statistics good={good} bad={bad} neutral={neutral}/>
-
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={()=>handleVote(votes, selected)}>vote</button>
+      <button onClick={()=>handleNext()}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>this has {votes.join(' ')}</p>
+      <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
     </div>
   )
 }
