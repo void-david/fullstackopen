a225cac6a768d6224440af0352b28557c36bf9c0
David A. Lozano <107713321+void-david@users.noreply.github.com>
2025-07-25 18:28:49 -0600

    advancing in part 1 d

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,18 @@ const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   const [counter, setCounter] = useState(0)
+
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
+
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
+
+
   console.log('rendering with counter value', counter)
 
   const increaseByOne = () => {
@@ -60,6 +72,12 @@ const App = () => {
 
   return (
     <div>
+      <div>
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
+      </div>
       <Display counter={counter}/>
       <Button
         onClick={increaseByOne}
